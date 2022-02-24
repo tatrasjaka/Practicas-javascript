be8c28fff0836b9accd9f411e1aac81a88aaa616
tatrasjaka <wmiiguel3@gmail.com>
2022-02-24 10:11:35 +0100

    cosas

---
+++ calculadora.js
@@ -0,0 +1,29 @@
+function sumarNumeros (a,b) /*sin parametros, tu los marcas abajo*/
+{
+let suma=parseInt(a)+parseInt(b);
+document.write(suma);
+}
+
+
+function restarNumeros (a,b) /*sin parametros, tu los marcas abajo*/
+{
+let resta=parseInt(a)- parseInt(b);
+document.write(resta);
+}
+
+
+function multiNumeros (a,b) /*sin parametros, tu los marcas abajo*/
+{
+let multiplicacion=parseInt(a)* parseInt(b);
+document.write(multiplicacion);
+}
+
+
+function diviNumeros (a,b) /*sin parametros, tu los marcas abajo*/
+{
+let division=parseInt(a)/ parseInt(b);
+document.write(division);
+}
+
+
+	
